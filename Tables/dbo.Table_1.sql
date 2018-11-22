93759436ca4602719d644dfafade4157cdbacc05
Rob <warnerrj79@icloud.com>
2018-11-22 16:51:11 +0000

    tfy

--- Tables/dbo.Table_1.sql
+++ Tables/dbo.Table_1.sql
@@ -1,5 +1,6 @@
 CREATE TABLE [dbo].[Table_1]
 (
-[colTest1] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
+[colTest1] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[colTest2] [varchar] (10) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
