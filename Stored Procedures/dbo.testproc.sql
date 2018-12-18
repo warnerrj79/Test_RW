042e59c8f8cf62a0521149cc7c9d73930c394719
Rob <warnerrj79@icloud.com>
2018-12-18 17:07:29 +0000

    Test amend and create SP

---
+++ Stored Procedures/dbo.testproc.sql
@@ -0,0 +1,8 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+create proc [dbo].[testproc]
+as
+SELECT * from dbo.george
+GO
--- Tables/dbo.george.sql
+++ Tables/dbo.george.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[george]
 (
 [id] [int] NOT NULL IDENTITY(1, 1),
-[col1] [varchar] (10) COLLATE Latin1_General_CI_AS NULL
+[col1] [varchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[col2] [varchar] (10) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
