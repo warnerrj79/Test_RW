a83b2661fd696150b814e3225a75bb1ff05571ac
Rob <warnerrj79@icloud.com>
2019-01-04 15:53:25 +0000

    Added 2 more rows

--- Data/dbo.Petros_Data.sql
+++ Data/dbo.Petros_Data.sql
@@ -1,4 +1,8 @@
 SET IDENTITY_INSERT [dbo].[Petros] ON
+INSERT INTO [dbo].[Petros] ([ID], [name]) VALUES (4, 'Giles')
+INSERT INTO [dbo].[Petros] ([ID], [name]) VALUES (5, 'Beth')
+SET IDENTITY_INSERT [dbo].[Petros] OFF
+SET IDENTITY_INSERT [dbo].[Petros] ON
 INSERT INTO [dbo].[Petros] ([ID], [name]) VALUES (1, 'Matt')
 INSERT INTO [dbo].[Petros] ([ID], [name]) VALUES (2, 'Robin')
 INSERT INTO [dbo].[Petros] ([ID], [name]) VALUES (3, 'Petros')
