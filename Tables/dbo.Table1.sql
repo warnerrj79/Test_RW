7107ae8135c09dfbebb7f6ed839e3083fd1e4df1
Rob <warnerrj79@icloud.com>
2019-01-07 12:02:28 +0000

    new table for new branch

--- Tables/dbo.Table1.sql
+++ Tables/dbo.Table1.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Table1]
 (
 [ID] [int] NOT NULL IDENTITY(1, 1),
-[Col1] [varchar] (20) COLLATE Latin1_General_CI_AS NULL
+[Col1] [varchar] (20) COLLATE Latin1_General_CI_AS NULL,
+[col2] [varchar] (20) COLLATE Latin1_General_CI_AS NOT NULL CONSTRAINT [DF__Table1__col2__4E53A1AA] DEFAULT ('xxx')
 ) ON [PRIMARY]
 GO
